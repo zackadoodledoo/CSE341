d35befb1d1484c3df49e9c2c2df563bc1b4e55dc
zackadoodledoo <sin12014@byui.edu>
2026-01-09 14:55:56 -0800

    updates 2.0

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+const routes = require('express').Router();
+
+routes.get('/', (req, res, next) => {
+    res.json('Awesome person');
+}))
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
